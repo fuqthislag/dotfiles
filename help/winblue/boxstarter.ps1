7d8b71d33cbeec13a94c6828acc9623dc383e60e
fuqthislag <fuqthislag@users.noreply.github.com>
2016-02-16 03:36:04 +0200

    minor tweaks

--- help/winblue/boxstarter.ps1
+++ help/winblue/boxstarter.ps1
@@ -18,6 +18,21 @@ $env:PSModulePath = $env:PSModulePath + ";%userprofile%\AppData\Local\atom\bin"
 
 apm install color-picker file-icons language-batch language-generic-config language-ini language-powershell language-viml minimap minimap-cursorline minimap-pigments multi-cursor-plus pigments quick-highlight sort-lines
 
+#color-picker
+#file-icons
+#language-batch
+#language-generic-config
+#language-ini
+#language-powershell
+#language-viml
+#minimap
+#minimap-cursorline
+#minimap-pigments
+#multi-cursor-plus
+#pigments
+#quick-highlight
+#sort-lines
+
 Install-WindowsUpdate -acceptEula
 
-## START http://boxstarter.org/package/url?d:\boxstarter.ps1
+# START http://boxstarter.org/package/url?d:\boxstarter.ps1
