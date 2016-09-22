dd7c29b68820d8a4fb61bf4b28f1236f8926542a
fuqthislag <fuqthislag@users.noreply.github.com>
2016-09-22 16:00:56 +0300

    autologin small change

--- help/autologin-vconsole.sh
+++ help/autologin-vconsole.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
-sudo touch /etc/systemd/system/getty@tty1.service.d/override.conf
+mkdir -p /etc/systemd/system/getty@tty1.service.d/
+touch /etc/systemd/system/getty@tty1.service.d/override.conf
 # Get username to variable
 _user=$(ls /home)
 # Autologin to vconsole
-sudo echo "[Service]
+echo "[Service]
 ExecStart=
 ExecStart=-/usr/bin/agetty --autologin $_user --noclear %I \$TERM" > /etc/systemd/system/getty@tty1.service.d/override.conf
