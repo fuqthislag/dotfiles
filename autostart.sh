051e6a4d744cac0bbbf9a27d4d6520befcef4e45
fuqthislag <fuqthislag@users.noreply.github.com>
2016-10-27 11:10:52 +0300

    more minor fixes

--- autostart.sh
+++ autostart.sh
@@ -2,10 +2,12 @@
 
 # Custom GPU Fan Control
 /home/aggour/dotfiles/autogpufan.sh &
-#Mouse Acceleration Off
+# Mouse Acceleration Off
 xset m 0 0
-#Screen Blanking Off
+# Touchpad
+#synclient VertTwoFingerScroll=0 VertEdgeScroll=1 TapButton1=1 TapButton2=3 TapButton3=2
+# Screen Blanking Off
 xset s off
-#Suspend Screen after 900 seconds (15 minutes)
+# Suspend Screen after 900 seconds (15 minutes)
 sleep 10
 xset dpms 0 0 900
