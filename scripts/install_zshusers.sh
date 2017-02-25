2f8f125ee61d0a55ccd8f0436a6a1ca665c8c2ee
fuqthislag <fuqthislag@users.noreply.github.com>
2017-02-25 13:55:34 +0200

    added permission to zsh users script

--- scripts/install_zshusers.sh
+++ scripts/install_zshusers.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/bin/sh
 
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
