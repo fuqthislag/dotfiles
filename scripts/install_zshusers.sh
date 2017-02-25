d1c65e6ddb1fae2a9ba7fab808ad323078761e4d
fuqthislag <fuqthislag@users.noreply.github.com>
2017-02-25 13:51:45 +0200

    minor changes to zsh users script

--- scripts/install_zshusers.sh
+++ scripts/install_zshusers.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 echo "plugins=(git sudo zsh-completions zsh-syntax-highlighting zsh-autosuggestions)
 autoload -U compinit && compinit" >> ~/.zshrc
