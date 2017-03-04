7a659061223a0a74c7db75bcaf369edca7c02fcf
fuqthislag <fuqthislag@users.noreply.github.com>
2017-03-04 16:43:07 +0200

    edited autostart script

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,11 +4,11 @@ guy=$(cd /home/* && pwd)
 # Custom GPU Fan Control
 $guy/dotfiles/scripts/autogpufan.sh &
 # Mouse Acceleration Off
-xset m 0 0
+#xset m 0 0 
 # Touchpad
 # synclient VertTwoFingerScroll=0 VertEdgeScroll=1 TapButton1=1 TapButton2=3 TapButton3=2
 # Screen Blanking Off
-xset s off
+#xset s off
 # Suspend Screen after 900 seconds (15 minutes)
-sleep 10
-xset dpms 0 0 900
+#sleep 10
+#xset dpms 0 0 900
